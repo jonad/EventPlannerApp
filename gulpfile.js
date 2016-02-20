6ce7487ca728645f3e5ea6a19e8bbe8dd91f1ebb
jonad <jonadjons@gmail.com>
2016-02-20 03:42:20 -0800

    Created gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var minifyCss = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var imageMin = require('gulp-imagemin');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+
+
+
+gulp.task('images', function() {
+    gulp.src(['src/img/**/*'])
+        .pipe(imageMin())
+        .pipe(gulp.dest('dist/img'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function() {
+    var b = browserify({
+        entries: 'src/js/main.js',
+        debug:true
+    });
+
+    b.bundle()
+        .pipe(source('main.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps:true}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/js/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('styles', function(){
+    gulp.src(['src/css/**/*.css'])
+            .pipe(sourcemaps.init())
+            .pipe(minifyCss())
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('dist/css'))
+            .pipe(browserSync.stream());
+
+});
+
+gulp.task('libs', function() {
+    gulp.src(['libs/**/*'])
+        .pipe(gulp.dest('dist/libs/'));
+});
+
+gulp.task('index', function() {
+    gulp.src(['src/index.html'])
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('templates', function() {
+    gulp.src(['src/templates/**/*.html'])
+        .pipe(gulp.dest('dist/templates/'));
+});
+
+gulp.task('default', ['styles', 'images', 'scripts', 'templates','libs',  'index'], function() {
+    browserSync.init({
+        server:'dist/'
+    });
+
+    gulp.watch('src/**/*', browserSync.reload);
+    gulp.watch('src/js/**/*.css', ['styles']  );
+    gulp.watch('src/css/**/*.js', ['scripts']);
+    gulp.watch('src/templates/**/*.html', ['templates']);
+    gulp.watch('src/index.html', ['index']);
+    gulp.watch('lib/**/*', ['lib']);
+
+
+    gulp.watch('dist/*.html', browserSync.reload());
+});
